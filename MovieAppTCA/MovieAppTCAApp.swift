14fbab34a8d2c0dd281f699ea4563db13553ba96
Yaroslav <yaroslavgolinsiy@gmail.com>
2025-04-08 21:45:27 +0200

    first commit

---
+++ MovieAppTCA/MovieAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTCAApp.swift
+//  MovieAppTCA
+//
+//  Created by Yaroslav Golinskiy on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
